4a12e89b6ea6d8ccd8ebfae1b9755ededde57123
Darkless12 <dftlg@yahoo.com>
2026-02-05 15:32:59 +0000

    What horrible semantics...

--- cpp08/ex02/MutantStack.hpp
+++ cpp08/ex02/MutantStack.hpp
@@ -1,6 +1,25 @@
 #ifndef MUTANTSTACK_HPP
 # define MUTANTSTACK_HPP
 
+#include <stack>
 
+template <typename T>
+class MutantStack : public std::stack<T>{
+	private:
+	public:
+		typedef typename std::stack<T>::container_type::iterator iterator;
+
+		MutantStack();
+		MutantStack(const MutantStack &other);
+		MutantStack &operator=(const MutantStack &other);
+		~MutantStack();
+
+
+		iterator begin();
+
+		iterator end();
+};
+
+# include "MutantStack.tpp"
 
 #endif
--- cpp08/ex02/main.cpp
+++ cpp08/ex02/main.cpp
@@ -1,5 +1,6 @@
-#include "MutantStack.hpp"
 #include <iostream>
+
+#include "MutantStack.hpp"
 
 int main(){
 	MutantStack<int> mstack;
@@ -24,3 +25,66 @@ int main(){
 	std::stack<int> s(mstack);
 	return 0;
 }
+
+
+/*#include <list>
+
+int main(){
+	std::list<int> mstack;
+
+	mstack.push_back(5);
+	mstack.push_back(17);
+
+	std::cout << mstack.back() << std::endl;
+	mstack.pop_back();
+	std::cout << mstack.size() << std::endl;
+
+	mstack.push_back(3);
+	mstack.push_back(5);
+	mstack.push_back(737);
+	//[...]
+	mstack.push_back(0);
+
+	std::list<int>::iterator it = mstack.begin();
+	std::list<int>::iterator ite = mstack.end();
+	++it;
+	--it;
+	while (it != ite){
+		std::cout << *it << std::endl;
+		++it;
+	}
+	return 0;
+}*/
+
+/*#include <vector>
+#include <iostream>
+
+int main() {
+	std::vector<int> vec;
+
+	vec.push_back(5);
+	vec.push_back(17);
+
+	std::cout << vec.back() << std::endl;   // equivalente a top()
+	vec.pop_back();
+
+	std::cout << vec.size() << std::endl;
+
+	vec.push_back(3);
+	vec.push_back(5);
+	vec.push_back(737);
+	vec.push_back(0);
+
+	std::vector<int>::iterator it = vec.begin();
+	std::vector<int>::iterator ite = vec.end();
+
+	++it;
+	--it;
+
+	while (it != ite) {
+		std::cout << *it << std::endl;
+		++it;
+	}
+
+	return 0;
+}*/
