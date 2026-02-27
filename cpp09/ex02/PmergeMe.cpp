fa4cd45b51a7bca9b3b4fd4889f6e2b740bc5de2
Darkless12 <dftlg@yahoo.com>
2026-02-27 09:19:09 +0000

    I'm busy. It's slow moving

---
+++ cpp09/ex02/PmergeMe.cpp
@@ -0,0 +1,14 @@
+#include "PmergeMe.hpp"
+
+
+PMergeMe::PMergeMe(std::string line){
+}
+
+PMergeMe::PMergeMe(const PMergeMe &other){
+}
+
+PMergeMe &	PMergeMe::operator=(const PMergeMe &other){
+}
+
+PMergeMe::~PMergeMe(){
+}
--- cpp09/ex02/PmergeMe.hpp
+++ cpp09/ex02/PmergeMe.hpp
@@ -1,9 +1,21 @@
 #ifndef PMERGEME_HPP
 # define PMERGEME_HPP
 
+# include <vector>
+# include <deque>
+# include <string>
+
 class PMergeMe {
 	private:
+		std::vector<int>	vec;
+		std::deque<int>		deq;
+
+		void	importValues(std::string line);
 	public:
+		PMergeMe(std::string line);
+		PMergeMe(const PMergeMe &other);
+		PMergeMe &operator=(const PMergeMe &other);
+		~PMergeMe();
 };
 
 #endif
